901c8ce077c3014173a3a4f77bfae1904cd3c585
unknown <znellsnorps@outlook.com>
2022-11-25 19:16:23 +0000

    Added damage flash to enemy sprite.

--- Scripts/Mob.gd
+++ Scripts/Mob.gd
@@ -8,10 +8,12 @@ func _on_Area2D_area_entered(area):
 		$AnimatedSprite.play("dead")
 		emit_signal("hit")
 		
+		$AnimatedSprite.material.set_shader_param("active", true)
 		get_tree().paused = true
 		var time_in_seconds = 0.1
 		yield(get_tree().create_timer(time_in_seconds), "timeout")
 		get_tree().paused = false
+		$AnimatedSprite.material.set_shader_param("active", false)
 
 # Declare member variables here. Examples:
 # var a = 2
